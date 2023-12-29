b6236287603692a71c2c7bb018b5d902a6718fc1
Orvin <orvin.tritama@hotmail.com>
2023-12-28 21:16:30 -0800

    finished exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,58 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGoodClick = () => {
     const updatedGood = good + 1;
     setGood(updatedGood);
+
+    const total = updatedGood+neutral+bad;
+    setTotal(total);
+    
+    const average = calculateAverage(updatedGood, neutral, bad, total);
+    setAverage(average);
+
+    const positive = calculatePositive(updatedGood, total);
+    setPositive(positive);
   }
   
   const handleNeutralClick = () => {
     const updatedNeutral = neutral + 1;
     setNeutral(updatedNeutral);
+
+    const total = updatedNeutral + good + bad;
+    setTotal(total);
+
+    const average = calculateAverage(good, updatedNeutral, bad, total);
+    setAverage(average);
+
+    const positive = calculatePositive(good, total);
+    setPositive(positive);
   }
   
   const handleBadClick = () => {
     const updatedBad = bad + 1;
     setBad(updatedBad);
+
+    const total = updatedBad + good + neutral;
+    setTotal(total);
+
+    const average = calculateAverage(good, neutral, updatedBad, total);
+    setAverage(average);
+
+    const positive = calculatePositive(good, total);
+    setPositive(positive);
+  }
+
+  const calculateAverage = (goodCount, neutralCount, badCount, total) => {
+    return (goodCount*1 + neutralCount*0 + badCount*-1) / total;
+  }
+
+  const calculatePositive = (goodCount, total) => {
+    return (goodCount / total) * 100;
   }
 
   return (
@@ -31,6 +69,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average}</p>
+      <p>positive {positive}%</p>
     </div>
   )
 }
